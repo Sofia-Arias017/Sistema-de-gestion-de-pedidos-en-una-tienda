b38aaf9bdb24af24ca646a368b93f365c351dc2c
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-03-24 04:21:10 -0500

    feat: :lipstick: Se agrego el array donde almacenar los pedidos y la  funcion de agregar un nuevo pedido

---
+++ sistema-de-gestion-de-pedidos-en-una-tienda/main.js
@@ -0,0 +1,40 @@
+let pedidos = []
+
+function agregarPedido() {
+let id = Number(prompt("Ingrese el ID del pedido:"))
+let nombreCliente = prompt("Ingrese el nombre del cliente:")
+let correoCliente = prompt("Ingrese el correo del cliente:")
+
+let productos = []
+let continuar
+do {
+    let nombreProducto = prompt("Ingrese el nombre del producto:")
+    let precioProducto = Number(prompt("Ingrese el precio del producto:"))
+    let cantidadProducto = Number(prompt("Ingrese la cantidad:"))
+
+    productos.push({
+        nombre: nombreProducto,
+        precio: precioProducto,
+        cantidad: cantidadProducto
+        })
+
+continuar = prompt("¿Desea agregar otro producto? (sí/no)").toLowerCase()
+    } while (continuar === "sí")
+
+let fecha = prompt("Ingrese la fecha del pedido (dd/mm/yyyy):")
+let estado = prompt("Ingrese el estado del pedido (pendiente/enviado/entregado):").toLowerCase()
+
+let pedido = {
+    id: id,
+    cliente: {
+        nombre: nombreCliente,
+        correo: correoCliente
+        },
+        productos: productos,
+        fecha: fecha,
+        estado: estado
+    };
+
+pedidos.push(pedido)
+console.log("Pedido agregado con éxito.")
+}
